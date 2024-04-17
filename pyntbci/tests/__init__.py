62e9a4b7e0d9743945888445ccb44e6c52c77700
Jordy Thielen <jordy.thielen@donders.ru.nl>
2024-04-17 22:04:50 +0200

    add gating tests

--- pyntbci/tests/__init__.py
+++ pyntbci/tests/__init__.py
@@ -1,4 +1,5 @@
 from pyntbci.tests.classifiers import *
+from pyntbci.tests.gating import *
 from pyntbci.tests.stimulus import *
 from pyntbci.tests.stopping import *
 from pyntbci.tests.transformers import *
